31dd91aeb2289a6495c023eabf796ba226c6ce4d
Soriggan <luisgdesouzac@gmail.com>
2020-07-14 22:36:50 -0300

    Arrow Functions

--- bundle.js
+++ bundle.js
@@ -4,23 +4,12 @@ var arr = [1, 2, 3, 4, 5, 6];
 var cesta = ["banana", "maçã", "abacate", "abacaxi"];
 var squareArr = arr.map(function (item) {
   return item * item;
-});
-var fruitPosition = cesta.map(function (item, index) {
-  "A/O ".concat(item, " est\xE1 na posi\xE7\xE3o ").concat(index, " do array.");
 }); // console.log(squareArr);
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-var filter2 = cesta.filter(function (item) {
-  return item === "banana";
-}); // console.log(filter);
-// console.log(filter2);
+var teste = function teste() {
+  return {
+    nome: "dirceu"
+  };
+};
 
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
+console.log(teste());
--- main.js
+++ main.js
@@ -1,33 +1,10 @@
 const arr = [1, 2, 3, 4, 5, 6];
 const cesta = ["banana", "maçã", "abacate", "abacaxi"];
 
-const squareArr = arr.map(function (item) {
-  return item * item;
-});
-
-const fruitPosition = cesta.map(function (item, index) {
-  `A/O ${item} está na posição ${index} do array.`;
-});
+const squareArr = arr.map((item) => item * item);
 
 // console.log(squareArr);
 
-const sum = arr.reduce(function (total, next) {
-  return total + next;
-});
+const teste = () => ({ nome: "dirceu" });
 
-const filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-
-const filter2 = cesta.filter(function (item) {
-  return item === "banana";
-});
-
-// console.log(filter);
-// console.log(filter2);
-
-const find = arr.find(function (item) {
-  return item === 4;
-});
-
-console.log(find);
+console.log(teste());
